6ffbdaf7dc8255b2c32ddcf57cb539062092fe59
preich <preich@192.168.1.101>
2020-07-10 08:33:16 +0200

    extract classes and test-classes dirs

--- src/main/java/com/github/jazzmuesli/ckjm/gradle/plugin/MetricsTask.java
+++ src/main/java/com/github/jazzmuesli/ckjm/gradle/plugin/MetricsTask.java
@@ -1,29 +1,23 @@
 package com.github.jazzmuesli.ckjm.gradle.plugin;
 
-import org.apache.bcel.util.ClassPath;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-import org.gradle.api.DefaultTask;
-import org.gradle.api.Project;
-import org.gradle.api.logging.Logger;
-import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.SourceSetContainer;
-import org.gradle.api.tasks.TaskAction;
-
-
-import gr.spinellis.ckjm.MetricsFilter;
-
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.apache.bcel.util.ClassPath;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.compile.JavaCompile;
+
+import gr.spinellis.ckjm.MetricsFilter;
 
 public class MetricsTask extends DefaultTask {
 
@@ -40,32 +34,44 @@ public class MetricsTask extends DefaultTask {
 	@TaskAction
 	void processDirectories() {
 		try {
-
 			String projectDir = project.getProjectDir().getAbsolutePath();
-			getLog().info("buildDir: " + project.getBuildDir());
-//			Path path = Paths.get(projectDir, "sourceDirs.csv");
-//			System.out.println("projectDir: " + projectDir + ", csvPath: " + path);
-//			CSVPrinter printer = new CSVPrinter(Files.newBufferedWriter(path),
-//					CSVFormat.DEFAULT.withHeader("sourceSetName", "dirName", "processed").withSystemRecordSeparator()
-//							.withDelimiter(';'));
-//
-//			SourceSetContainer sourceSets = project.getConvention().getPlugin(JavaPluginConvention.class)
-//					.getSourceSets();
-//
-//			for (Map.Entry<String, SourceSet> entry : sourceSets.getAsMap().entrySet()) {
-//				SourceSet sourceSet = entry.getValue();
-//				Set<File> srcDirs = sourceSet.getAllJava().getSrcDirs();
-//				System.out.println("Processing sourceSet " + entry.getKey() + " with srcDirs=" + srcDirs);
-//				for (File srcDir : srcDirs) {
-//					boolean processed = processSourceDirectory(srcDir.getAbsolutePath());
-//					printer.printRecord(entry.getKey(), srcDir.getAbsolutePath(), processed);
-//					printer.flush();
-//				}
-//			}
-//			printer.close();
+			System.out.println("projectDir: " + projectDir);
+			System.out.println("buildDir: " + project.getBuildDir());
+			ClassPath cp = new ClassPath();
+			System.out.println("classpath: " + cp);
+			Set<String> mainOutputs = getOutput("compileJava");
+			for (String f : mainOutputs) {
+				cp = new ClassPath(cp, f);
+			}
+			Set<String> testOutputs = getOutput("compileTestJava");
+			for (String f : testOutputs) {
+				cp = new ClassPath(cp, f);
+			}
+			
+	        final ClassPath fcp = cp;
+	        mainOutputs.forEach(dir -> {
+	        	System.out.println("mainDir: " + dir);
+	        	processSourceDirectory(fcp, dir);
+	        });
+			
+	        testOutputs.forEach(dir -> {
+	        	System.out.println("testDir: " + dir);
+	        	processSourceDirectory(fcp, dir);
+	        });
+			
 		} catch (Exception e) {
 			getLog().error(e.getMessage(), e);
 		}
+	}
+
+	protected Set<String> getOutput(String task) {
+		Set<String> outputs = getProject().getTasks().getByName(task).getOutputs().getFiles().getFiles()
+				.stream().filter(p->p.exists()).map(x->x.getAbsolutePath()).collect(Collectors.toSet());
+		return outputs;
+	}
+
+	protected FileCollection getClasspath(Task task) {
+		return ((JavaCompile)task).getClasspath();
 	}
 
 	protected void processSourceDirectory(ClassPath cp, String dirName) {
